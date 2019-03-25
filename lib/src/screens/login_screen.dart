c8c8891255d9087cb999b8bad2559607d7924822
runcycle <justinbartholomew@gmail.com>
2019-03-25 16:45:18 -0400

    add email field

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -10,7 +10,30 @@ class LoginScreenState extends State<LoginScreen> {
   Widget build(context) {
     return Container(
       margin: EdgeInsets.all(20.0),
-      child: Text('Login Screen!'),
+      child: Form(
+        child: Column(
+          children: [
+            emailField(),
+            // passwordField(),
+            // submitButton(),
+          ],
+        ),
+      ),
     );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      ),
+    );
+  }
+  Widget passwordField() {
+
+  }
+  Widget submitButton() {
+
   }
 }
